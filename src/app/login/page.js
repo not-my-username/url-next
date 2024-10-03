09381601419e3e729f6d238eb58789b206bebcaf
Liam <liambrackensherwin@gmail.com>
2024-10-03 15:48:15 +1300

    change db

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,5 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
-
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
@@ -8,5 +7,9 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Root URL handling
 export default async function Page() {
-
+  var { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "github",
+  });
+  console.log(data);
+  console.log(error);
 }
